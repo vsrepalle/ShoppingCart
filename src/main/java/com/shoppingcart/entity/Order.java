4ada3249bc04fc15a656eacf8f2e6fafdce24c1a
vsrepalle <viswa.repalle@gmail.com>
2022-05-15 23:05:07 +0530

    Shopping Cart

--- src/main/java/com/shoppingcart/entity/Order.java
+++ src/main/java/com/shoppingcart/entity/Order.java
@@ -1,6 +1,8 @@
 package com.shoppingcart.entity;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 @Entity
 @Table(name= "order_new")
--- src/main/java/com/shoppingcart/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/shoppingcart/service/impl/ShoppingCartServiceImpl.java
@@ -11,7 +11,6 @@ import com.shoppingcart.repository.UserRepository;
 import com.shoppingcart.service.ShoppingCartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
