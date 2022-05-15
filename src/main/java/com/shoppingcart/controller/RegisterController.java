a32fdcd36694b07b083be24e662eb0d2ff088d4d
vsrepalle <viswa.repalle@gmail.com>
2022-05-15 15:16:03 +0530

    Shopping Cart

--- src/main/java/com/shoppingcart/controller/RegisterController.java
+++ src/main/java/com/shoppingcart/controller/RegisterController.java
@@ -12,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping(value = "users")
 public class RegisterController {
 
-	
+
 	private AccountRepository accountRepository;
 
 	@Autowired
@@ -27,20 +29,23 @@ public class RegisterController {
 
 	@PostMapping(value = "/account")
 	public ResponseEntity<?> register(@Valid @RequestBody Account account) {
-		if(account.getEmail()==null||account.getEmail().isEmpty()) {
-			return new ResponseEntity<>("Email Is Required",HttpStatus.BAD_REQUEST);
+		if (null != account.getName() && !isValidUsername(account.getName())) {
+			return new ResponseEntity<>("Name Shouldn't contain anything other than alphabets", HttpStatus.BAD_REQUEST);
+		}
+		if (account.getEmail() == null || account.getEmail().isEmpty()) {
+			return new ResponseEntity<>("Email Is Required", HttpStatus.BAD_REQUEST);
 		}
 		Optional<Account> accountFromDB = accountRepository.findByEmail(account.getEmail());
 		if (!accountFromDB.isPresent()) {
-			
-			if(account.getName() == null || account.getEmail() == null || account.getPassword() == null || 
+
+			if (account.getName() == null || account.getEmail() == null || account.getPassword() == null ||
 					account.getConfirmPassword() == null ||
-					account.getName().isEmpty() || account.getEmail().isEmpty() || account.getPassword().isEmpty() || 
+					account.getName().isEmpty() || account.getEmail().isEmpty() || account.getPassword().isEmpty() ||
 					account.getConfirmPassword().isEmpty()) {
-					return new ResponseEntity<>("All fields are required", HttpStatus.BAD_REQUEST);
-				
+				return new ResponseEntity<>("All fields are required", HttpStatus.BAD_REQUEST);
+
 			}
-			
+
 			if (account.getPassword().equals(account.getConfirmPassword())) {
 				if (account.getWishList() != null) {
 					return new ResponseEntity<>("Only Registered Account can add wishlist", HttpStatus.BAD_REQUEST);
@@ -54,5 +59,13 @@ public class RegisterController {
 		return new ResponseEntity<>("Email Already Exists Please Log In", HttpStatus.BAD_REQUEST);
 	}
 
-
+	private static boolean isValidUsername(String name) {
+		if (name == null) {
+			return false;
+		}
+		String regex = "[a-zA-Z][a-zA-Z ]*";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(name);
+		return m.matches();
+	}
 }
