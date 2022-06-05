cd9d2c4a842f88bf9a2be99b29b8716d5eac1182
vsrepalle <viswa.repalle@gmail.com>
2022-06-05 22:19:04 +0530

    Fixed some bugs in Low Stock Product

--- src/main/java/com/shoppingcart/repository/ProductRepository.java
+++ src/main/java/com/shoppingcart/repository/ProductRepository.java
@@ -17,7 +17,7 @@ public interface ProductRepository extends JpaRepository<Product, Integer>{
 	List<Product> findByProductName(@Param("prodName") String prodName);
 	List<Product> findByCategory(String category);
 
-	@Query("select p from Product p where p.stockQty=min(p.stockQty)")
+	@Query("select p from Product p group by p.stockQty having p.stockQty=min(p.stockQty)")
 	Optional<Product> getLowStockProduct();
 
 }
