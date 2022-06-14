3d907e1e6e762aae6dfb51a29b6ad6bc24dee0ab
vsrepalle <viswa.repalle@gmail.com>
2022-06-14 15:42:55 +0530

    added some junit methods

--- src/test/java/com/shoppingcart/repository/AccountRepositoryTest.java
+++ src/test/java/com/shoppingcart/repository/AccountRepositoryTest.java
@@ -3,29 +3,31 @@ package com.shoppingcart.repository;
 import com.shoppingcart.entity.Account;
 import com.shoppingcart.mapper.AccountMapper;
 import com.shoppingcart.request.AccountRequest;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.annotation.Rollback;
+
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
-@RunWith(SpringRunner.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AccountRepositoryTest {
 
-    @Autowired
-    private TestEntityManager entityManager;
+
 
     @Autowired
     private AccountRepository accountRepository;
 
     @Test
     @DisplayName("Find By Role")
+    @Order(1)
     void findByRole() {
         AccountRequest accountRequest = new AccountRequest();
         accountRequest.setName("Nani");
@@ -33,13 +35,14 @@ class AccountRepositoryTest {
         accountRequest.setEmail("nani@gmail.com");
         accountRequest.setPassword("Nani1234@");
         accountRequest.setConfirmPassword("Nani1234@");
-        entityManager.persist(AccountMapper.mapToAccount(accountRequest));
+        accountRepository.save(AccountMapper.mapToAccount(accountRequest));
         List<Account> accounts = accountRepository.findByRole("USER");
         assertNotNull(accounts.get(0));
     }
 
     @Test
     @DisplayName("Find By Email")
+    @Order(2)
     void findByEmail() {
         AccountRequest accountRequest = new AccountRequest();
         accountRequest.setName("Nani");
@@ -47,5 +50,8 @@ class AccountRepositoryTest {
         accountRequest.setEmail("nani@gmail.com");
         accountRequest.setPassword("Nani1234@");
         accountRequest.setConfirmPassword("Nani1234@");
+        accountRepository.save(AccountMapper.mapToAccount(accountRequest));
+        Optional<Account> account = accountRepository.findByEmail(accountRequest.getEmail());
+        assertTrue(account.isPresent());
     }
 }
